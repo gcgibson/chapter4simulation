e1c3e83f32e9f3c10ccffb73eb2fe4ffd663607e
graham gibson <gcgibson@umass.edu>
2020-09-08 13:37:20 -0600

    update

--- sim.R
+++ sim.R
@@ -1,20 +1,39 @@
 ## deterministic
 
-parameters <- c(beta=1.1,gamma=.2)
+parameters <- c(beta=.8,gamma=.2)
 simulate_discrete <- function(t,parameters){
-  delta_t <- 1e-6
   i_prime <- c(1)
-  s <- c(100000)
+  s <- c(1000000000)
   i <- c(1)
   for (t_ in 2:t){
-     i_prime <- c(i_prime,parameters[1]*s[t_-1]*i[t_-1]*delta_t)
-     s <- c(s,s[t_-1] - i_prime[t_])
-     i <- c(i,i[t_-1] + i_prime[t_] + parameters[2]*i[t_-1]*delta_t)
+     i_prime <- c(i_prime,parameters[1]*(s[t_-1]/s[1])*i[t_-1])
+     s <- c(s,max(s[t_-1] - i_prime[t_],0))
+     i <- c(i,i[t_-1] + i_prime[t_] - parameters[2]*i[t_-1])
      
   }
-  ret_df <- data.frame(i_prime=i_prime,s=s)
+  ret_df <- data.frame(i_prime=i_prime,s=s,i=i)
   return (ret_df)
 } 
+out <- simulate_discrete(100,parameters)
+plot(out$i_prime)
+plot(out$i)
+
+simulate_random<- function(t,parameters){
+  i_prime <- c(1)
+  s <- c(1000000000)
+  i <- c(1)
+  for (t_ in 2:t){
+    i_prime <- c(i_prime,rpois(n=1,lambda=parameters[1]*(s[t_-1]/s[1])*i[t_-1]))
+    s <- c(s,max(s[t_-1] - i_prime[t_],0))
+    i <- c(i,i[t_-1] + i_prime[t_] - rpois(n=1,lambda=parameters[2]*i[t_-1]))
+    
+  }
+  ret_df <- data.frame(i_prime=i_prime,s=s,i=i)
+  return (ret_df)
+} 
+out <- simulate_random(100,parameters)
+plot(out$i_prime)
+plot(out$i)
 
 
 
@@ -23,15 +42,14 @@ cori_estimator <- function(I_prime,parameters){
   for (t in 2:length(I_prime)){
     p_sum <-0 
     for (i in 1:(t-1)){
-      p_sum <-p_sum + I_prime[t-i]*parameters[2]*(1-parameters[2])**(i)
+      p_sum <-p_sum + I_prime[t-i]*parameters[2]*(1-parameters[2])**(i-1)
     }
     r_t <- c(r_t,I_prime[t]/p_sum)
   }
   return (r_t)
 }
 
-out <- simulate_discrete(500,parameters)
-plot(out$i_prime)
+
 r_t_hat <- cori_estimator(out$i_prime,parameters)
 plot(r_t_hat,ylim=c(0,parameters[1]/parameters[2]+1))
 lines(parameters[1]/parameters[2]*out$s/out$s[1],col='red')
